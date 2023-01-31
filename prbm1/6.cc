70dbce96ca26c16d2985382849e1fd5ed9bdfed9
Justin Stitt <jstitt007@gmail.com>
2023-01-30 19:23:15 -0800

    update

--- prbm1/6.cc
+++ prbm1/6.cc
@@ -1,19 +1,38 @@
 /* Group #6 */
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int AwayFrom13(int num){
   // Return absolute value of num - 13
-  return std::abs(num-13);
+  return abs(num-13);
 }
+
+int addTwo(int a, int b) {
+  return a + b;
+}
+//F(x) = 2*x + 4
+
+float something() {
+  return 3.14;
+}
+
+string addBond(string name) {
+  return name + " Bond";
+}
+
 
 int main() {
   // Take in user input
-  int userNum;
-  std::cin >> userNum;
+  int userNum; // variable
+  cin >> userNum;
 
   // Call on Function and print num away from 13
-  std::cout << AwayFrom13(userNum);
+  cout << AwayFrom13(userNum);
 
-  return 0;
-
+  string my_name = "James";
+  string spy_name = addBond(my_name);
+  std::cout << my_name + " Bond" << "\n";
+  return 0; // status code A-OK
 }
